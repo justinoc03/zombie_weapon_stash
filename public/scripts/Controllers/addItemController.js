40a6b69e24bdeb753d0552cec0deedfbd68c9311
Justin O'Connor <justin.oc03@gmail.com>
2016-10-11 15:06:06 -0500

    uploaders both play nicely together

--- public/scripts/Controllers/addItemController.js
+++ public/scripts/Controllers/addItemController.js
@@ -2,26 +2,62 @@ myApp.controller("addItemController", ['$scope', '$http', function($scope, $http
   console.log('In addItemController');
 
   var imgurData = {};
+  var picFromCam;
 
   ////////////////////////////////////File Uploader/////////////////////////////////////////
   $scope.uploadFile = function(){
-      
 
       var file = $scope.myFile;
       console.dir(file);
 
-      $http({
-        url: 'https://api.imgur.com/3/image',
-        method: 'POST',
-        headers: {Authorization: 'Client-ID 3ed9fd5c8fff37e'},
-        data: file,
-        dataType: 'JSON'
-      }).then(function(imgurResponse){
-          console.log('success from Imgur:', imgurResponse);
-          imgurData = imgurResponse;
+      if(file === undefined && picFromCam !== undefined){
+        $http({
+          url: 'https://api.imgur.com/3/image',
+          method: 'POST',
+          headers: {Authorization: 'Client-ID 3ed9fd5c8fff37e'},
+          data: picFromCam,
+          dataType: 'JSON'
+        }).then(function(imgurResponse){
+            console.log('success from Imgur:', imgurResponse);
+            imgurData = imgurResponse;
 
-          $scope.addItemToDB();
-        });
+            $scope.addItemToDB();
+          });
+      } else if(file !== undefined && picFromCam === undefined){
+        $http({
+          url: 'https://api.imgur.com/3/image',
+          method: 'POST',
+          headers: {Authorization: 'Client-ID 3ed9fd5c8fff37e'},
+          data: file,
+          dataType: 'JSON'
+        }).then(function(imgurResponse){
+            console.log('success from Imgur:', imgurResponse);
+            imgurData = imgurResponse;
+
+            $scope.addItemToDB();
+          });
+        } else if(file !== undefined && picFromCam !== undefined){
+          alert('Delete picture or select choose photo and press "cancel" to clear uploader ')
+      }else {
+          alert('Please take picture or upload an image...');
+          return false;
+      }
+
+
+
+      // $http({
+      //   url: 'https://api.imgur.com/3/image',
+      //   method: 'POST',
+      //   headers: {Authorization: 'Client-ID 3ed9fd5c8fff37e'},
+      //   data: picFromCam,
+      //   dataType: 'JSON'
+      // }).then(function(imgurResponse){
+      //     console.log('success from Imgur:', imgurResponse);
+      //     imgurData = imgurResponse;
+      //
+      //     $scope.addItemToDB();
+      //   });
+      // }
   };
 
   /////////////////////////////Add New Item//////////////////////////////////////////
@@ -55,135 +91,140 @@ myApp.controller("addItemController", ['$scope', '$http', function($scope, $http
 
   ////////////////////////////////////Camera App/////////////////////////////////////////
   //this camera app comes from a free to use tutorial here: http://tutorialzine.com/2016/07/take-a-selfie-with-js/
-    $scope.init = function(){
-        console.log( 'in cameraFunction' );
+  $scope.init = function(){
+      console.log( 'in cameraFunction' );
 
-          // References to all the element we will need.
-          var video = document.querySelector('#camera-stream'),
-              image = document.querySelector('#snap'),
-              start_camera = document.querySelector('#start-camera'),
-              controls = document.querySelector('.controls'),
-              take_photo_btn = document.querySelector('#take-photo'),
-              delete_photo_btn = document.querySelector('#delete-photo'),
-              download_photo_btn = document.querySelector('#download-photo'),
-              error_message = document.querySelector('#error-message');
-
-
-          // The getUserMedia interface is used for handling camera input.
-          // Some browsers need a prefix so here we're covering all the options
-          navigator.getMedia = ( navigator.getUserMedia ||
-          navigator.webkitGetUserMedia ||
-          navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia);
+        // References to all the element we will need.
+        var video = document.querySelector('#camera-stream'),
+            image = document.querySelector('#snap'),
+            start_camera = document.querySelector('#start-camera'),
+            controls = document.querySelector('.controls'),
+            take_photo_btn = document.querySelector('#take-photo'),
+            delete_photo_btn = document.querySelector('#delete-photo'),
+            download_photo_btn = document.querySelector('#download-photo'),
+            error_message = document.querySelector('#error-message');
 
 
-          if(!navigator.getMedia){
-              displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
-          }
-          else{
-              // Request the camera.
-              navigator.getMedia(
-                  {
-                      video: true
-                  },
-                  // Success Callback
-                  function(stream){
-                      // Create an object URL for the video stream and
-                      // set it as src of our HTLM video element.
-                      video.src = window.URL.createObjectURL(stream);
-
-                      // Play the video element to start the stream.
-                      video.play();
-                      video.onplay = function() {
-                          showVideo();
-                      };
-                  },
-                  // Error Callback
-                  function(err){
-                      displayErrorMessage("There was an error with accessing the camera stream: " + err.name, err);
-                  }
-              );
-          }
+        // The getUserMedia interface is used for handling camera input.
+        // Some browsers need a prefix so here we're covering all the options
+        navigator.getMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
 
 
-          // Mobile browsers cannot play video without user input,
-          // so here we're using a button to start it manually.
-          start_camera.addEventListener("click", function(e){
-              e.preventDefault();
-              // Start video playback manually.
-              video.play();
-              showVideo();
-          });
+        if(!navigator.getMedia){
+            displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
+        }
+        else{
+            // Request the camera.
+            navigator.getMedia(
+                {
+                    video: true
+                },
+                // Success Callback
+                function(stream){
+                    // Create an object URL for the video stream and
+                    // set it as src of our HTLM video element.
+                    video.src = window.URL.createObjectURL(stream);
 
-          take_photo_btn.addEventListener("click", function(e){
-              e.preventDefault();
-              var snap = takeSnapshot();
-              // Show image.
-              image.setAttribute('src', snap);
-              image.classList.add("visible");
-              // Enable delete and save buttons
-              delete_photo_btn.classList.remove("disabled");
-              download_photo_btn.classList.remove("disabled");
-              // Set the href attribute of the download button to the snap url.
-              download_photo_btn.href = snap;
-              // Pause video playback of stream.
-              video.pause();
-          });
+                    // Play the video element to start the stream.
+                    video.play();
+                    video.onplay = function() {
+                        showVideo();
+                    };
+                },
+                // Error Callback
+                function(err){
+                    displayErrorMessage("There was an error with accessing the camera stream: " + err.name, err);
+                }
+            );
+        }
 
-          delete_photo_btn.addEventListener("click", function(e){
-              e.preventDefault();
-              // Hide image.
-              image.setAttribute('src', "");
-              image.classList.remove("visible");
-              // Disable delete and save buttons
-              delete_photo_btn.classList.add("disabled");
-              download_photo_btn.classList.add("disabled");
-              // Resume playback of stream.
-              video.play();
-          });
 
-          function showVideo(){
-              // Display the video stream and the controls.
-              hideUI();
-              video.classList.add("visible");
-              controls.classList.add("visible");
-          }
+        // Mobile browsers cannot play video without user input,
+        // so here we're using a button to start it manually.
+        start_camera.addEventListener("click", function(e){
+            e.preventDefault();
+            // Start video playback manually.
+            video.play();
+            showVideo();
+        });
 
-          function takeSnapshot(){
-              // Here we're using a trick that involves a hidden canvas element.
-              var hidden_canvas = document.querySelector('canvas'),
-                  context = hidden_canvas.getContext('2d');
-              var width = video.videoWidth,
-                  height = video.videoHeight;
-              if (width && height) {
-                  // Setup a canvas with the same dimensions as the video.
-                  hidden_canvas.width = width;
-                  hidden_canvas.height = height;
-                  // Make a copy of the current frame in the video on the canvas.
-                  context.drawImage(video, 0, 0, width, height);
-                  // Turn the canvas image into a dataURL that can be used as a src for our photo.
-                  return hidden_canvas.toDataURL('image/png');
-              }
-          }
+        take_photo_btn.addEventListener("click", function(e){
+            e.preventDefault();
+            var snap = takeSnapshot();
+            // Show image.
+            image.setAttribute('src', snap);
+            image.classList.add("visible");
+            // Enable delete and save buttons
+            delete_photo_btn.classList.remove("disabled");
+            download_photo_btn.classList.remove("disabled");
+            // Set the href attribute of the download button to the snap url.
+            download_photo_btn.href = snap;
+            // Pause video playback of stream.
+            video.pause();
+            picFromCam = download_photo_btn.href.split(',')[1];
+            console.log('picFromCam:', picFromCam);
+        });
 
-          function displayErrorMessage(error_msg, error){
-              error = error || "";
-              if(error){
-                  console.error(error);
-              }
-              error_message.innerText = error_msg;
-              hideUI();
-              error_message.classList.add("visible");
-          }
+        delete_photo_btn.addEventListener("click", function(e){
+            e.preventDefault();
+            // Hide image.
+            image.setAttribute('src', "");
+            image.classList.remove("visible");
+            // Disable delete and save buttons
+            delete_photo_btn.classList.add("disabled");
+            download_photo_btn.classList.add("disabled");
+            //clear picFromCam
+            picFromCam = undefined;
+            // Resume playback of stream.
+            console.log('picFromCam:', picFromCam);
+            video.play();
+        });
 
-          function hideUI(){
-              // Helper function for clearing the app UI.
-              controls.classList.remove("visible");
-              start_camera.classList.remove("visible");
-              video.classList.remove("visible");
-              snap.classList.remove("visible");
-              error_message.classList.remove("visible");
-          }
-    };//end camera init
+        function showVideo(){
+            // Display the video stream and the controls.
+            hideUI();
+            video.classList.add("visible");
+            controls.classList.add("visible");
+        }
+
+        function takeSnapshot(){
+            // Here we're using a trick that involves a hidden canvas element.
+            var hidden_canvas = document.querySelector('canvas'),
+                context = hidden_canvas.getContext('2d');
+            var width = video.videoWidth,
+                height = video.videoHeight;
+            if (width && height) {
+                // Setup a canvas with the same dimensions as the video.
+                hidden_canvas.width = width;
+                hidden_canvas.height = height;
+                // Make a copy of the current frame in the video on the canvas.
+                context.drawImage(video, 0, 0, width, height);
+                // Turn the canvas image into a dataURL that can be used as a src for our photo.
+                return hidden_canvas.toDataURL('image/png');
+            }
+        }
+
+        function displayErrorMessage(error_msg, error){
+            error = error || "";
+            if(error){
+                console.error(error);
+            }
+            error_message.innerText = error_msg;
+            hideUI();
+            error_message.classList.add("visible");
+        }
+
+        function hideUI(){
+            // Helper function for clearing the app UI.
+            controls.classList.remove("visible");
+            start_camera.classList.remove("visible");
+            video.classList.remove("visible");
+            snap.classList.remove("visible");
+            error_message.classList.remove("visible");
+        }
+  };//end camera init
 
 }]);//end addItemController
